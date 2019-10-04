ec87c197cf8fc41cc76de67c585c04c6e65a3453
Ruber Hernandez <ruberhenandez@gmail.com>
2019-10-03 20:08:35 -0500

    --save

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,7 +12,7 @@ import '../assets/styles/App.scss';
 const API = 'http://localhost:3000/initalState';
 
 
-const App = () => {
+const Home = () => {
     
     const initialState = useInitialState(API);
     console.log(initialState)
@@ -58,4 +58,4 @@ const App = () => {
 
     )}
 };
-export default App;
+export default Home;
