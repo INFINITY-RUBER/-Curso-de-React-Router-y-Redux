79f2c7966e18f96bd4e62def945ab9b3c12173f8
Ruber Hernandez <ruberhenandez@gmail.com>
2019-10-09 23:37:22 -0500

    se implementa gravatar y sale TypeError

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,42 +1,84 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import { loginRequest } from '../actions';
 import '../assets/styles/components/Login.scss';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
+// import { connect } from 'http2';
 
 
-const Login = () => (
+const Login = props => {
+    const [form, setValues] = useState({
+        email: '',
+    });
 
-<section className="login">
-    <section className="login__container">
-        <h2>Inicia sesión</h2>  
-        <form className="login__container--form">
-            <input className="input" type="password" placeholder="Contraseña"/>
-            <input className="input" type="text" placeholder="Correo"/>
-            <button className="button">Iniciar sesión</button>
-            <div className="login__container--remember-me">
-                <label>
-                    <input type="checkbox" id="cbox1" value="first_checkbox" /> Recuérdame
-                </label>
-                <a href="/">Olvidé mi contraseña</a>
-        </div>
-        </form>
+    const handleInput = event => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        props.loginRequest(form);
+        props.history.push('/')
+        // console.log();
+    }
+
+    return (
+
+    <section className="login">
+        <section className="login__container">
+            <h2>Inicia sesión</h2>  
+            <form className="login__container--form" onSubmit={handleSubmit} >
+                
+                <input 
+                    name="password"
+                    className="input" 
+                    type="text" 
+                    placeholder="Correo"
+                    onChange={handleInput}
+
+                />
+                <input 
+                    name="email"
+                    className="input" 
+                    type="password" 
+                    placeholder="Contraseña"
+                    onChange={handleInput}
+                />
+                <button className="button">Iniciar sesión</button>
+                <div className="login__container--remember-me">
+                    <label>
+                        <input type="checkbox" id="cbox1" value="first_checkbox" /> Recuérdame
+                    </label>
+                    <a href="/">Olvidé mi contraseña</a>
+            </div>
+            </form>
 
 
-    <section className="login__container--social-media">
-        <div><img src={googleIcon} /> Inicia sesión con Google</div>
-        <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
+        <section className="login__container--social-media">
+            <div><img src={googleIcon} /> Inicia sesión con Google</div>
+            <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
+        </section>
+        <p className="login__container--register">
+            No tienes ninguna cuenta 
+            <Link to="/register">
+                Regístrate
+            </Link>
+        </p>
+        </section>
     </section>
-    <p className="login__container--register">
-        No tienes ninguna cuenta 
-        <Link to="/register">
-            Regístrate
-        </Link>
-    </p>
-    </section>
-</section>
 
 );
+};
 
+const mapDispatchToProps = {
+    loginRequest,
+};
 
-export default Login;
+// export default Login;
+export default connect(null, mapDispatchToProps)(Login);
+
